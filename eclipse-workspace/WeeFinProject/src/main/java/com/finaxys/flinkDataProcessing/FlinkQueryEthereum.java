070397b114b39983bd949de2c4c28acdc691be69
Christophe <cblampain@finaxys.com>
2018-08-07 15:14:01 +0200

    Added Flink processing class

--- eclipse-workspace/WeeFinProject/src/main/java/com/finaxys/flinkDataProcessing/FlinkQueryEthereum.java
+++ eclipse-workspace/WeeFinProject/src/main/java/com/finaxys/flinkDataProcessing/FlinkQueryEthereum.java
@@ -2,34 +2,30 @@ package com.finaxys.flinkDataProcessing;
 
 import java.util.Properties;
 
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+
+import com.finaxys.model.Blocks;
+import com.finaxys.schema.BlocksSchema;
 
 public class FlinkQueryEthereum {
 
 	public static void main(String[] args) throws Exception {
 
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		StreamTableEnvironment tableEnv = TableEnvironment.getTableEnvironment(env);
 		// configure Kafka consumer
 		Properties props = new Properties();
 		props.setProperty("bootstrap.servers", "localhost:9092"); // Broker default host:port
-		props.setProperty("group.id", "test"); // Consumer group ID
+		props.setProperty("group.id", "flink-consumer"); // Consumer group ID
 
-		DataStream<String> stream = env
-				.addSource(new FlinkKafkaConsumer011<String>(args[0], new SimpleStringSchema(), props));
+		DataStream<Blocks> stream = env
+				.addSource(new FlinkKafkaConsumer011<String>(args[0], new BlocksSchema(), props));
 
-		stream.map(new MapFunction<String, String>() {
-			private static final long serialVersionUID = -6867736771747690202L;
-
-			@Override
-			public String map(String value) throws Exception {
-				return "Stream Value: " + value;
-			}
-		}).print();
+		
 
 		env.execute();
 
